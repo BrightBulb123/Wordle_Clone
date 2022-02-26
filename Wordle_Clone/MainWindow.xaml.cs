69800ee2a66a35b62198d9a01d9f3e9844e64c8f
BrightBulb123 <83759628+BrightBulb123@users.noreply.github.com>
2022-02-26 21:37:58 +1100

    Changed all if statements to switch (for inputting text)

--- Wordle_Clone/MainWindow.xaml.cs
+++ Wordle_Clone/MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace Wordle_Clone {
         int currentRow = 0;
         int currentColumn = 0;
 
-
         public MainWindow() {
             InitializeComponent();
         }
@@ -47,54 +46,101 @@ namespace Wordle_Clone {
         private void Fill_Box(char character) {
             lblDebug.Content = character;
 
-            if (currentRow == 0) {
-                if (currentColumn == 0) { TR0C0.Text = character.ToString(); }
-                else if (currentColumn == 1) { TR0C1.Text = character.ToString(); }
-                else if (currentColumn == 2) { TR0C2.Text = character.ToString(); }
-                else if (currentColumn == 3) { TR0C3.Text = character.ToString(); }
-                else if (currentColumn == 4) { TR0C4.Text = character.ToString(); }
-                currentColumn++;
+            (int, int) rowColumn = (currentRow, currentColumn);
+
+            switch (rowColumn) {
+                case (0, 0):
+                    TR0C0.Text = character.ToString();
+                    break;
+                case (0, 1):
+                    TR0C1.Text = character.ToString();
+                    break;
+                case (0, 2):
+                    TR0C2.Text = character.ToString();
+                    break;
+                case (0, 3):
+                    TR0C3.Text = character.ToString();
+                    break;
+                case (0, 4):
+                    TR0C4.Text = character.ToString();
+                    break;
+                case (1, 0):
+                    TR1C0.Text = character.ToString();
+                    break;
+                case (1, 1):
+                    TR1C1.Text = character.ToString();
+                    break;
+                case (1, 2):
+                    TR1C2.Text = character.ToString();
+                    break;
+                case (1, 3):
+                    TR1C3.Text = character.ToString();
+                    break;
+                case (1, 4):
+                    TR1C4.Text = character.ToString();
+                    break;
+                case (2, 0):
+                    TR2C0.Text = character.ToString();
+                    break;
+                case (2, 1):
+                    TR2C1.Text = character.ToString();
+                    break;
+                case (2, 2):
+                    TR2C2.Text = character.ToString();
+                    break;
+                case (2, 3):
+                    TR2C3.Text = character.ToString();
+                    break;
+                case (2, 4):
+                    TR2C4.Text = character.ToString();
+                    break;
+                case (3, 0):
+                    TR3C0.Text = character.ToString();
+                    break;
+                case (3, 1):
+                    TR3C1.Text = character.ToString();
+                    break;
+                case (3, 2):
+                    TR3C2.Text = character.ToString();
+                    break;
+                case (3, 3):
+                    TR3C3.Text = character.ToString();
+                    break;
+                case (3, 4):
+                    TR3C4.Text = character.ToString();
+                    break;
+                case (4, 0):
+                    TR4C0.Text = character.ToString();
+                    break;
+                case (4, 1):
+                    TR4C1.Text = character.ToString();
+                    break;
+                case (4, 2):
+                    TR4C2.Text = character.ToString();
+                    break;
+                case (4, 3):
+                    TR4C3.Text = character.ToString();
+                    break;
+                case (4, 4):
+                    TR4C4.Text = character.ToString();
+                    break;
+                case (5, 0):
+                    TR5C0.Text = character.ToString();
+                    break;
+                case (5, 1):
+                    TR5C1.Text = character.ToString();
+                    break;
+                case (5, 2):
+                    TR5C2.Text = character.ToString();
+                    break;
+                case (5, 3):
+                    TR5C3.Text = character.ToString();
+                    break;
+                case (5, 4):
+                    TR5C4.Text = character.ToString();
+                    break;
             }
-            else if (currentRow == 1) {
-                if (currentColumn == 0) { TR1C0.Text = character.ToString(); }
-                else if (currentColumn == 1) { TR1C1.Text = character.ToString(); }
-                else if (currentColumn == 2) { TR1C2.Text = character.ToString(); }
-                else if (currentColumn == 3) { TR1C3.Text = character.ToString(); }
-                else if (currentColumn == 4) { TR1C4.Text = character.ToString(); }
-                currentColumn++;
-            }
-            else if (currentRow == 2) {
-                if (currentColumn == 0) { TR2C0.Text = character.ToString(); }
-                else if (currentColumn == 1) { TR2C1.Text = character.ToString(); }
-                else if (currentColumn == 2) { TR2C2.Text = character.ToString(); }
-                else if (currentColumn == 3) { TR2C3.Text = character.ToString(); }
-                else if (currentColumn == 4) { TR2C4.Text = character.ToString(); }
-                currentColumn++;
-            }
-            else if (currentRow == 3) {
-                if (currentColumn == 0) { TR3C0.Text = character.ToString(); }
-                else if (currentColumn == 1) { TR3C1.Text = character.ToString(); }
-                else if (currentColumn == 2) { TR3C2.Text = character.ToString(); }
-                else if (currentColumn == 3) { TR3C3.Text = character.ToString(); }
-                else if (currentColumn == 4) { TR3C4.Text = character.ToString(); }
-                currentColumn++;
-            }
-            else if (currentRow == 4) {
-                if (currentColumn == 0) { TR4C0.Text = character.ToString(); }
-                else if (currentColumn == 1) { TR4C1.Text = character.ToString(); }
-                else if (currentColumn == 2) { TR4C2.Text = character.ToString(); }
-                else if (currentColumn == 3) { TR4C3.Text = character.ToString(); }
-                else if (currentColumn == 4) { TR4C4.Text = character.ToString(); }
-                currentColumn++;
-            }
-            else if (currentRow == 5) {
-                if (currentColumn == 0) { TR5C0.Text = character.ToString(); }
-                else if (currentColumn == 1) { TR5C1.Text = character.ToString(); }
-                else if (currentColumn == 2) { TR5C2.Text = character.ToString(); }
-                else if (currentColumn == 3) { TR5C3.Text = character.ToString(); }
-                else if (currentColumn == 4) { TR5C4.Text = character.ToString(); }
-                currentColumn++;
-            }
+            currentColumn++;
 
             if (currentColumn >= 5) {
                 currentRow++;
